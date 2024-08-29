0a58e273ba8f5e01424b3cbdb2fb0e5ed9ec4df3
pransh1 <mauryapransh2@gmail.com>
2024-08-29 15:40:56 +0530

    first commit

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const {auth,validator} = require("./allmiddlewares")
+const {auth,validator} = require("./allmiddlewares");
 // router.use(express.json())
 router.use((req,res,next)=>{
   console.log("This is nested middleware")
